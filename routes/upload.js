7b4a6ba381a80a8a3f74993a7dad93d93b6d1eb3
sanghwa1 <sanghwa@catenoid.net>
2024-04-29 00:51:12 +0900

    Feat: 업로드 진행중

--- routes/upload.js
+++ routes/upload.js
@@ -3,25 +3,32 @@ const AWS = require("aws-sdk");
 const { getCurrentInvoke } = require('@codegenie/serverless-express')
 
 const router = express.Router();
-const s3 = new AWS.S3();
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+});
 const STORAGE_BUCKET_NAME = `sanghwa-test`; 
 AWS.config.update({ region: "ap-northeast-2" });
 
-/* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/signed-url', async function(req, res, next) {
   try{
     const {event, context} = getCurrentInvoke();
-    // const arrFileKey = ['06b7b805-32a6-468f-a0ab-7058374a1ff5_1920x1080 (1).png'];
-    const arrSignedUrl = {};
-    for await (const fileKeyVal of arrFileKey) {
-      const params = { Bucket: STORAGE_BUCKET_NAME, Key: '06b7b805-32a6-468f-a0ab-7058374a1ff5_1920x1080 (1).png' };
+    const fileNames = ['KakaoTalk_Photo_2024-02-05-16-58-57 004.jpeg'];
+    const signedUrls = {};
+    for await (const fileName of fileNames) {
+      const params = { 
+        Bucket: STORAGE_BUCKET_NAME, 
+        Key: fileName,
+        Expires: 60 * 1,
+        ContentType: 'image/*'
+      };
       const signedUrl = await s3.getSignedUrlPromise("getObject", params)
-      arrSignedUrl[fileKeyVal] = signedUrl;
+      signedUrls[fileName] = signedUrl;
     }
-    res.json(...arrSignedUrl)
+    res.json(signedUrls)
   } catch(err){
     console.log('err - ', err);
   }
 });
+
 
 module.exports = router;
